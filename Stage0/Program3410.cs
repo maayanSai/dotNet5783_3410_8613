5714571032da98a65f682914c1db7526caf626cb
מעיין סעידוב <maayan18403@gmail.com>
2022-11-01 20:17:43 +0200

    Inital code + Welcome functions

---
+++ Stage0/Program3410.cs
@@ -0,0 +1,17 @@
+﻿using System;
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome3410();
+        Welcome8613();
+        Console.ReadKey();
+    }
+    static partial void Welcome8613();
+    private static void Welcome3410()
+    {
+        Console.WriteLine("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", userName);
+    }
+}
