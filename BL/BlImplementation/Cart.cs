ef6cb122e61a8e17c4dead5a4bd505a20a414d63
ayala aftergut <tmunot1234567@gmail.com>
2022-12-06 22:00:23 +0200

    curt end(bo)

--- BL/BlImplementation/Cart.cs
+++ BL/BlImplementation/Cart.cs
@@ -7,6 +7,7 @@ using System.Transactions;
 
 internal class Cart:ICart
     {
+    private static readonly Random _rnd = new();
     DalApi.IDal Dal = new Dal.DalList();
     public BO.Cart? Update(BO.Cart cart, int id, int amount)
     {
@@ -19,6 +20,23 @@ internal class Cart:ICart
         if (amount == 0)
             cart.Items.Remove(boOrdi);
         if(amount <boOrdi .Amount )
+        {
+            cart.Items.Remove(boOrdi);
+            cart.TotalPrice-=(boOrdi.Amount-amount)*boOrdi.Price;
+            boOrdi.Amount = amount;
+            boOrdi.Totalprice=boOrdi.Price*amount;
+            cart.Items.Add(boOrdi); 
+        }
+        if (amount >boOrdi.Amount)
+        {
+            cart.Items.Remove(boOrdi);
+            cart.TotalPrice+=(amount-boOrdi.Amount)*boOrdi.Price;  
+            boOrdi.Amount = amount;
+            boOrdi.Totalprice=boOrdi.Price*amount;
+            cart.Items.Add(boOrdi);
+
+        }
+        return cart;
 
 
 
@@ -75,9 +93,80 @@ internal class Cart:ICart
 
     public void MakeAnOrder(BO.Cart cart)
     {
-        throw new NotImplementedException();
+        //כל המוצרים קיימים(לפי התעודת זהות,למרות שיכול להיות שהם קיימים עם כמות אפס
+        try
+        {
+            cart?.Items?.ForEach(x => Dal.Product.GetById(x.ProductID));
+        }
+        catch(DO.DalDoesNotExistException exp)
+        {
+            throw new Exceptions.BODoesNotExistException(exp.Message);   
+        }
+        if (cart.Items.Exists(x => x.Amount<=0))
+            throw new Exceptions.BlInvalidInputException("negetive amount");
+        if (cart.Items.Exists(x => x.Amount>Dal.Product.GetById(x.ProductID).InStock))
+            throw new Exception("therse not enogth instook");
+        if (cart.CustomerName==null)
+            throw new Exceptions.BlInvalidInputException("missing name");
+        if(cart.CustomerAdress==null)
+            throw new Exceptions.BlInvalidInputException("missing adress");
+        if(cart .CustomerEmail==null)
+            throw new Exceptions.BlInvalidInputException("missing Email");
+        // if(cart.CustomerEmail)-כתובת לפי פורמט
+        // throw new Exceptions.BlInvalidInputException("invlavel email");
+        int days = _rnd.Next(21, 200);
+        int orderId;
+        DO.Order ord = new()
+        {
+            CustomerAdress = cart.CustomerAdress,
+            CustomerEmail=cart.CustomerEmail,
+            CustomerName=cart.CustomerName,
+            ID=1,
+            OrderDate=DateTime.Now.AddDays(-days),
+            ShipDate=DateTime.MinValue,
+            DeliveryrDate=DateTime.MinValue,
+            Amount=cart.Items.Select(x => x.Amount).Sum(),
+        };
+
+
+        orderId=Dal.Order.Add(ord);//לא צריך לבדוק כי אין חריגה להוספה של אורדר
+        foreach(var bordi in cart.Items)
+        {
+            DO.OrderItem dOrdi = new()
+            {
+                ID=bordi.ID,//זניח
+                ProductID=bordi.ProductID,
+                OrderID=orderId,
+                Price=bordi.Price,
+                Amount=bordi.Amount,
+            };
+            Dal.OrderItem.Add(dOrdi); //לא יכולה להיות חריגה
+            DO.Product p;
+            try
+            {
+                 p= Dal.Product.GetById(dOrdi.ID);
+            }
+            catch (DO.DalDoesNotExistException exp)
+            {
+                throw new Exceptions.BODoesNotExistException(exp.Message);
+            }
+            p.InStock-=dOrdi.Amount;
+            try 
+            {
+                Dal.Product.Update(p);
+            }
+            catch (DO.DalDoesNotExistException exp)
+            {
+                throw new Exceptions.BODoesNotExistException(exp.Message);
+            }
+
+        }
+       
+
+
+
     }
 
- 
+
 }
 
