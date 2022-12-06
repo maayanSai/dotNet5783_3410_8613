a5c1f71f0d0d2dcb0094558cb159128ff1361338
מעיין סעידוב <maayan18403@gmail.com>
2022-12-06 20:20:33 +0200

    fix

--- BL/BO/OrderForList .cs
+++ BL/BO/OrderForList .cs
@@ -6,7 +6,7 @@ public class OrderForList
     public int ID { get; set; }
     public string? CustomerName { get; set; } // The name of customer of the order
     public OrderStatus Status { set; get; }
-    public double TotalPrice { set; get; }
+    public double? TotalPrice { set; get; }
     public int Amount { get; set; } // 
 
     // An action that prints the orderItem
--- BL/BO/OrderTracking.cs
+++ BL/BO/OrderTracking.cs
@@ -5,6 +5,5 @@ public class OrderTracking
 {
     public int ID { get; set; }
     public OrderStatus Status { set; get; }
-
     public List<Tuple<DateTime,string> >? Tracking { set; get; }
 }
