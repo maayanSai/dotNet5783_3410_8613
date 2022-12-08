827800883767845793b172c85561dfbcc1071135
ayala aftergut <tmunot1234567@gmail.com>
2022-12-08 21:55:57 +0200

    fix

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -2,10 +2,13 @@
 
     public class BlAlreadyExistEntityException : Exception
     {
-        public BlAlreadyExistEntityException(string message, Exception innerException) : base(message, innerException)
+    public BlAlreadyExistEntityException(string message) : base(message)
+    {
+    }
+    public BlAlreadyExistEntityException(string message, Exception innerException) : base(message, innerException)
         {
         }
-        public override string ToString() => base.ToString() + $"Entity is already exist.";
+    public override string ToString() => $"Entity is already exist." + base.ToString();
     }
 
 /// <summary>
@@ -14,88 +17,82 @@
 [Serializable]
 public class BlMissingEntityException : Exception
 {
-    string Message;
-    int a;
-    public BlMissingEntityException(string message, DO.DalMissingIdException innerException, int b = 0) : base(message, innerException)
+   
+    public BlMissingEntityException(string message, DO.DalMissingIdException innerException) : base(message, innerException)
     {
-        a=b;
+       
     }
-    public BlMissingEntityException(string message, int b = 1) : base()
-    {
-        Message=message;
-        a=b;
-    }
+    public BlMissingEntityException(string message) : base(message) { }
+  
     public override string ToString()
     {
-        if (a==0)
-            return InnerException.ToString() +Message +$"Missing Entity";
-        else
-            return $"{Message}";
+       return base.ToString() + "missing entitiy";
 
     }
 }
-    /// <summary>
-    /// if the Property null throw an exception
-    /// </summary>
-    [Serializable]
+
+/// <summary>
+/// if the Property null throw an exception
+/// </summary>
+[Serializable]
     public class BlNullPropertyException : Exception
     {
         public BlNullPropertyException(string message) : base(message) { }
-    }
-    /// <summary>
-    /// if its not the category throw an exception
-    /// </summary>
-    [Serializable]
-    public class BlWorngCategoryException : Exception
+    public override string ToString()
     {
-        public BlWorngCategoryException(string message) : base(message) { }
-    }
-    /// <summary>
-    /// if the date in correct throw an exception
-    /// </summary>
-    [Serializable]
-    public class BlIncorrectDatesException : Exception
-    {
-        public BlIncorrectDatesException(string message) : base(message) { }
-        /// <summary>
-        /// if the Property null throw an exception
-        /// </summary>
-        [Serializable]
-        public class BlNullPropertyException : Exception
-        {
-            public BlNullPropertyException(string message) : base(message) { }
-        }
-        /// <summary>
-        /// if its not the category throw an exception
-        /// </summary>
-        [Serializable]
-        public class BlWorngCategoryException : Exception
-        {
-           public BlWorngCategoryException(string message) : base(message) { }
-        }
-       
 
+        return "null property" + base.ToString();
+
+    }
 }
+
+
 /// <summary>
-/// if there is an empty string throw an exception
+/// if its not the category throw an exception
 /// </summary>
-
 [Serializable]
-    public class BlEmptyStringException : Exception
+public class BlWorngCategoryException : Exception
+{
+    public BlWorngCategoryException(string message) : base(message) { }
+    public override string ToString()
     {
-        public BlEmptyStringException(string? message) : base(message)
-        {
 
-        }
+        return "Wrong catefory" + base.ToString();
+
     }
-    /// <summary>
-    /// if its in correct throw an exception
-    /// </summary>
-    [Serializable]
-    public class BlInCorrectException : Exception
+}
+
+/// <summary>
+/// if the date in correct throw an exception
+/// </summary>
+[Serializable]
+public class BlIncorrectDatesException : Exception
+{
+    public BlIncorrectDatesException(string message) : base(message) { }
+    public override string ToString()
     {
-        public BlInCorrectException(string? message) : base(message) { }
+
+        return "date exeption" + base.ToString();//$"{Message}";
+
     }
+}
+
+
+
+
+public class BlInCorrectException : Exception
+{
+    //string Message1;
+    public BlInCorrectException(string? message) : base(message) { }// { //Message1=message; }
+    public override string ToString()
+    {
+
+        return " bl-incorrect" + base.ToString();//$"{message}";
+
+    }
+}
+
+
 
 
 
