bda0778720404b60adfc33c1e692be8043d57306
אילה אפטרגוט <tmunot1234567@gmail.com>
2023-01-11 14:49:52 +0200

    1

--- BL/Bllmplemenatation/Order.cs
+++ BL/Bllmplemenatation/Order.cs
@@ -43,19 +43,6 @@ internal class Order : IOrder
         };
     }
 
-    public BO.OrderForList GetOrderForList(int id)
-    {
-        DO.Order? order = dal.Order.GetById(id);
-        var items = dal.OrderItem.GetAll(delegate (DO.OrderItem? x) { return x?.OrderID == order?.ID; });
-        return new BO.OrderForList
-        {
-            ID = order?.ID ?? throw new BO.BlNullPropertyException("missing order id"),
-            CustomerName = order?.CustomerName ?? " ",
-            Status = GetStatus(order),
-            TotalPrice = items.Sum(x => x?.Price * x?.Amount ?? 0),//alredy cheked id isnot null
-            Amount = items.Count(),
-        };
-    }
 
     /// <summary>
     /// A private helper function, that returns the state of the order 
